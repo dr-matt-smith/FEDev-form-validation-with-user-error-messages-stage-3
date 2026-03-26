cdbcbc071956c23198a8cddf075b95dea8951377
Matt Smith <matt.smith@tudublin.ie>
2026-03-26 18:45:48 +0000

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -18,7 +18,7 @@ export const actions = {
         const data = await request.formData();
 
         try {
-            let description = data.get('description')
+            let description = data.get('description');
             db.createTodo(cookies.get('userid'), description);
 
             // LOG successful TODO creation
@@ -26,7 +26,7 @@ export const actions = {
 
         } catch (error) {
             // LOG caught ERROR
-            console.log("TODO - *** ERROR *** - there was a problem with your new TODO = '" +  + error.message + "'");
+            console.log("TODO - *** ERROR *** - there was a problem with your new TODO = '" + error.message + "'");
         }
     },
 
